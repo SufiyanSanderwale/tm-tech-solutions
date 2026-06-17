f9be5041117e44d91104c1ea591897793c51e3ab
SufiyanSanderwale <sufiyansanderwale54@gmail.com>
2026-06-17 13:22:39 +0530

    Update base URL in sitemap to new domain

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://tm-tech-solutions.vercel.app'
+  const baseUrl = 'https://www.tmtechsolutions.in'
   
   return [
     {
